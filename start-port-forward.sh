3dbe66120dbab49cb2aa5b8406212c5ad7034373
THANUJA <124047527+ThanujaRatakonda@users.noreply.github.com>
2025-12-11 06:50:06 +0000

    Update start-port-forward.sh
    /usr/local/bin

--- start-port-forward.sh
+++ start-port-forward.sh
@@ -13,8 +13,8 @@ done
 
 echo "Starting port-forwarding in background..."
 
-nohup /home/ThanujaRatakonda/kp_4/kubectl port-forward svc/backend 5000:5000 --address 0.0.0.0 > backend.log 2>&1 &
-nohup /home/ThanujaRatakonda/kp_4/kubectl port-forward svc/frontend 4000:3000 --address 0.0.0.0 > frontend.log 2>&1 &
-nohup /home/ThanujaRatakonda/kp_4/kubectl port-forward statefulset/database 5433:5432 --address 0.0.0.0 > database.log 2>&1 &
+nohup   /usr/local/bin/kubectl port-forward svc/backend 5000:5000 --address 0.0.0.0 > backend.log 2>&1 &
+nohup   /usr/local/bin/kubectl port-forward svc/frontend 4000:3000 --address 0.0.0.0 > frontend.log 2>&1 &
+nohup   /usr/local/bin/kubectl port-forward statefulset/database 5433:5432 --address 0.0.0.0 > database.log 2>&1 &
 
 echo "Port forwarding started."
